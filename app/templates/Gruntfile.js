b3e5e72759422251a876165991a05103a29471d9
Robert Yokota <ryokota@yammer-inc.com>
2014-02-05 23:06:20 -0800

    Fix Gruntfile

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -9,8 +9,8 @@ module.exports = function (grunt) {
   grunt.initConfig({
     yeoman: {
       // configurable paths
-      app: require('./bower.json').appPath || 'public',
-      dist: 'public'
+      app: require('./bower.json').appPath || 'resources/public',
+      dist: 'resources/public'
     },
     sync: {
       dist: {
@@ -60,7 +60,7 @@ module.exports = function (grunt) {
           middleware: function (connect) {
             return [
               proxySnippet,
-              connect.static(require('path').resolve('public'))
+              connect.static(require('path').resolve('resources/public'))
             ];
           }
         }
